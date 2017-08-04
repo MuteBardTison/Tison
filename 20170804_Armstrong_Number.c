07ea0ff6627d6d5cc6cc5dd09e71a1e4c54ca84a
Zihan Qi <z.qi@jacobs-university.de>
2017-08-05 00:06:07 +0800

    Add files via upload

---
+++ 20170804_Armstrong_Number.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+#include<math.h>
+int main(){
+    int i;
+    for(i=1;i<1000;i++){
+        if(i == pow((i/1000),3)+pow((i/100%10),3)+pow((i/10%10),3)+pow((i%10),3))
+            printf("%d\n", i);
+    }
+    return 0;
+}
